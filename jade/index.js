2d55fccd020b20e39bdc5b7e02a9e6c1cf2b7b77
cody1991 <476490767@qq.com>
2015-09-22 18:26:24 +0800

    add jade demo

---
+++ jade/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname);
+app.use(express.static(path.join(__dirname)))
+
+app.get('/', function(req, res) {
+    res.render('article', {
+        word: 'word'
+    });
+});
+
+
+var server = http.createServer(app);
+server.listen(3002);
+console.log('server started on localhost:3002');
