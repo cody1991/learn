2a90ad0b7e165da5c286780463c1a6f54d8ef119
cody1991 <476490767@qq.com>
2015-09-27 10:56:29 +0800

    add jade demo

--- jade-learning/index.js
+++ jade-learning/index.js
@@ -7,28 +7,31 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname)))
 
-var demoList = ['1-attributes','2-case'],
-	demoListLen = demoList.length;
+var demoList = ['1-attributes', '2-case', '3-code'],
+    demoListLen = demoList.length;
 
 app.get('/', function(req, res) {
     res.render('index', {
         pageTitle: 'hello jade',
         youAreUsingJade: true,
-        demoList :demoList
+        demoList: demoList
     });
 });
 
-
-demoList.forEach(function(value,index){
-	console.log(value)
-	app.get('/' + value,function(req,res){
-		res.render(value,{
-			pageTitle: value
-		})
-	})
+demoList.forEach(function(value, index) {
+    console.log(value)
+    app.get('/' + value, function(req, res) {
+        res.render(value, {
+            pageTitle: value
+        })
+    })
 });
 
-
 var server = http.createServer(app);
-server.listen(8080);
-console.log('server started on localhost:8080');
+var port = 8083;
+server.listen(port);
+console.log('server started on localhost:' + port);
+
+
+// lsof -i :3000
+// kill -9 <PID>
