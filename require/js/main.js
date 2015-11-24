5eac5de87f2c88b5eec268a3a879217b72838c63
cody1991 <476490767@qq.com>
2015-11-24 16:47:34 +0800

    add more demo

--- require/js/main.js
+++ require/js/main.js
@@ -1,15 +1,21 @@
 // config是一个js对象，常配置的有baseUrl和paths
 require.config({
-    baseUrl: './js/',
     paths: {
-        jquery: './jquery-2.1.4.min',
+        jqueryTransit: './jquery.transit.min',
+        jquery: [
+            'http://cdn.bootcss.com/jquery/2.1.4/jquery.min',
+            './jquery-2.1.4.min'
+        ],
         two: './two',
         three: './three'
+    },
+    shim: {
+        'jqueryTransit': ['jquery']
     }
 });
 
-require(['jquery'], function($) {
-    console.log($('body'));
+require(['jqueryTransit'], function(transit) {
+    console.log(transit);
 });
 
 // require(['two'], function(two) {
