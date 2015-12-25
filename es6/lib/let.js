3c73c6dcd31bb95f9f3caa4e471e467e2d835ec4
cody1991 <476490767@qq.com>
2015-12-25 14:48:17 +0800

    add new study and bookmarks

---
+++ es6/lib/let.js
@@ -0,0 +1,19 @@
+'use strict';
+
+{
+    let a = 10;
+    var b = 1;
+}
+
+console.log(b);
+// console.log(a); 会报错
+
+let arr = [1, 2, 3];
+
+for (let i = 0; i < arr.length; i++) {
+    setTimeout(function () {
+        console.log(i);
+    });
+}
+
+// console.log(i); 会报错
