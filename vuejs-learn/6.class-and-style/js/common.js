4e7bb09d66708c48fa7c12fb3633842198f8b14f
cody1991 <476490767@qq.com>
2016-01-07 20:00:59 +0800

    finish vue.js class and style

---
+++ vuejs-learn/6.class-and-style/js/common.js
@@ -0,0 +1,14 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        classObject: {
+            'class-a': true,
+            'class-b': false
+        },
+        classA: 'class-a',
+        classB: 'class-b',
+        isB: true,
+        activeColor: 'red',
+        fontSize: 30
+    }
+})
