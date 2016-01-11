fb639a3ccaeddfb0d12265da685f1868c92ca723
cody1991 <476490767@qq.com>
2016-01-11 12:31:46 +0800

    continue the components learn

--- vuejs-learn/12.components/js/common.js
+++ vuejs-learn/12.components/js/common.js
@@ -4,8 +4,9 @@
 
 // Vue.component('my-component', MyComponent);
 
-Vue.component('my-component',{
-    template:'<div>A custom component!</div>'
+Vue.component('my-component', {
+    template: '<div>{{msg}}</div>',
+    props: ['msg']
 })
 
 // var Parent = Vue.extend({
@@ -16,5 +17,38 @@ Vue.component('my-component',{
 //     }
 // })
 var vm = new Vue({
-    el: '#example'
+    el: '#example',
+    data: {
+        parentMsg: ''
+    }
 });
+
+
+Vue.component('child', {
+    template: '#child-template',
+    data: function() {
+        return {
+            msg: 'hello'
+        }
+    },
+    methods: {
+        notify: function() {
+            if (this.msg.trim()) {
+                this.$dispatch('child-msg', this.msg)
+                this.msg = ''
+            }
+        }
+    }
+})
+
+var parent = new Vue({
+    el: '#events-example',
+    data: {
+        messages: []
+    },
+    events: {
+        'child-msg': function(msg) {
+            this.messages.push(msg)
+        }
+    }
+})
