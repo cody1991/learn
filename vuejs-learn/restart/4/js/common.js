2f6c647b7762c84fcfd603e02f2a2b9cfd3b1649
cody1991 <476490767@qq.com>
2016-01-13 17:08:36 +0800

    add restart demo

---
+++ vuejs-learn/restart/4/js/common.js
@@ -0,0 +1,9 @@
+var demo1 = new Vue({
+    el: '#demo1',
+    data: {
+        msg1: 'a不能变化',
+        html1: '<b>123</b>',
+        number: 0,
+        ok: false
+    }
+});
---
+++ vuejs-learn/restart/6/js/common.js
@@ -0,0 +1,18 @@
+var demo1 = new Vue({
+    el: '#demo1',
+    data: {
+        isA: true,
+        isB: false,
+        classObject: {
+            'class-a': true,
+            'class-b': false
+        },
+        classA: 'class-a',
+        classB: 'class-b',
+        styleObject: {
+            color: 'red',
+            fontSize: '13px',
+            transform: 'scale(1.2)'
+        }
+    }
+})
