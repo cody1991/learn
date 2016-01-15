2c4fca38e55dcbb7a24510a15559f42a5ec69d51
cody1991 <476490767@qq.com>
2016-01-15 10:56:17 +0800

    finish modal demo

--- vuejs-learn/example/modal/js/common.js
+++ vuejs-learn/example/modal/js/common.js
@@ -1,13 +1,14 @@
 Vue.component('modal', {
-    template: '#modal-template',
-    props: {
-        show: {
-            type: Boolean,
-            required: true,
-            twoWay: true
+        template: '#modal-template',
+        props: {
+            show: {
+                type: Boolean,
+                required: true,
+                twoWay: true
+            }
         }
-    }
-});
+    })
+
 
 
 var modal = new Vue({
