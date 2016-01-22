05831e5d728434f6626f8d83ae2166cccf97968e
cody1991 <476490767@qq.com>
2016-01-22 15:57:38 +0800

    keeping python learning

--- python-learn/1/4-string-and-encode.py
+++ python-learn/1/4-string-and-encode.py
@@ -23,3 +23,19 @@ print(len('中文'))
 
 print(len(b'ABC'))
 print(len('中文'.encode('utf-8')))
+
+print('Hello, %s' % 'world')
+print('Hi, %s , you have $%d' % ('Michael',1000000))
+
+print('%.2f' % 3.1415926)
+
+print('%2d-%02d' % (3,1))
+
+print('Age: %s. Gender: %s' % (25,True))
+
+s1 = 72
+s2 = 85
+
+r = (s2 - s1) / s1 * 100
+
+print('%.2f%%' % r)
