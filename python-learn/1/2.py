41ad4fa46d97e391a513cc9f8e9391726848c9a9
cody1991 <476490767@qq.com>
2016-01-22 12:29:07 +0800

    keeping python learing

---
+++ python-learn/1/2.py
@@ -0,0 +1,7 @@
+# print absolute value of an integer
+a = -100
+
+if a >= 0:
+    print(a)
+else:
+    print(-a)
---
+++ python-learn/1/3-data-type.py
@@ -0,0 +1,69 @@
+print('I\'m ok.')
+
+print('I\'m learning\nPython.')
+
+print('\\\n\\')
+
+print('\\\t\\')
+
+print(r'\\\t\\')
+
+print('''line1
+...line2
+...line3''')
+
+print(True)
+print(False)
+print(3 > 2)
+print(3 < 2)
+
+print(True and True)
+print(True and False)
+print(False and False)
+print(5 > 3 and 3 > 1)
+
+print(True or True)
+print(True or False)
+print(False or False)
+print(5 > 3 or 1 > 3)
+
+print(not True)
+print(not False)
+print(not 1 > 2)
+
+age = 20
+
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print(10 / 3)
+print(9 / 3)
+print(10 // 3)
+print(10 % 3)
+
+n = 123
+print(n)
+
+f = 456.789
+print(f)
+
+s1 = 'Hello, world'
+print(s1)
+
+s2 = 'Hello, \'Adam\''
+print(s2)
+
+s3 = r'Hello,"Bart"'
+print(s3)
+
+s4 = r'''hello,
+Lisa!'''
+
+print(s4)
