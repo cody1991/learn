fed56be3eda3bd0c1dc81e0618c703a3dabe3f59
cody1991 <476490767@qq.com>
2016-02-23 12:27:12 +0800

    finish python base relearn

---
+++ python-learn/relearn/1-base/2-base.py
@@ -0,0 +1,23 @@
+print("I\'m ok.")
+print('I\'m learning\nPython.')
+print('\\\n\\')
+print(r'\\\n\\')
+print('''line1
+ line2
+line3 
+dsadasdasd''')
+
+print(True and True)
+print(True or False)
+print(not True)
+
+age = 20
+if(age > 18):
+    print('adult')
+else:
+    print('tennager')
+
+print('空值:' ,None)
+
+print(10 / 3)
+print(9 / 3)
---
+++ python-learn/relearn/1-base/7-loop.py
@@ -0,0 +1,8 @@
+names = ['body','cody','anna']
+for name in names:
+    print(name)
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
