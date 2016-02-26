3b2e91111921177bb7d44961891d5041269f2d21
cody1991 <476490767@qq.com>
2016-02-26 10:47:54 +0800

    finish python oop learn

---
+++ python-learn/relearn/7-oop-senior/6-metaclass.py
@@ -0,0 +1,8 @@
+from hello import Hello
+
+h = Hello()
+h.hello()
+
+# type()函数可以查看一个类型或变量的类型，Hello是一个class，它的类型就是type，而h是一个实例，它的类型就是class Hello。
+print(type(Hello))
+print(type(h))
---
+++ python-learn/relearn/7-oop-senior/hello.py
@@ -0,0 +1,3 @@
+class Hello(object):
+    def hello(self,name="world"):
+        print('Hello,%s' % name)
