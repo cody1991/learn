09d4a3673cf4be9cd2eb3c8214f64eda61c53453
cody1991 <476490767@qq.com>
2016-03-07 20:13:30 +0800

    start python process learn

---
+++ python-learn/relearn/10-process/2-subprocess.py
@@ -0,0 +1,44 @@
+import subprocess
+
+# subprocess模块可以让我们非常方便地启动一个子进程，然后控制其输入和输出。
+
+# 下面的例子演示了如何在Python代码中运行命令nslookup www.python.org
+
+print('$ nslookup www.python.org')
+
+r = subprocess.call(['nslookup','www.python.org'])
+print('Exit code:',r)
+
+# Process之间肯定是需要通信的，操作系统提供了很多机制来实现进程间的通信。Python的multiprocessing模块包装了底层的机制，提供了Queue、Pipes等多种方式来交换数据。
+
+# 我们以Queue为例，在父进程中创建两个子进程，一个往Queue里写数据，一个从Queue里读数据：
+
+from multiprocessing import Process,Queue
+import os,time,random
+
+def write(q):
+    print('Process to write:%s' % os.getpid())
+    for value in ['A','B','C']:
+        print('Put %s to queue...'  % value)
+        q.put(value)
+        time.sleep(random.random())
+
+def read(q):
+    print('Process to read : %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+if __name__=='__main__':
+    # 父进程创建Queue，并传给各个子进程：
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    # 启动子进程pw，写入:
+    pw.start()
+    # 启动子进程pr，读取:
+    pr.start()
+    # 等待pw结束:
+    pw.join()
+    # pr进程里是死循环，无法等待其结束，只能强行终止:
+    pr.terminate()
