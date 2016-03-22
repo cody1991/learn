0de5ef221c09fec383667aa4738a981f3807699e
cody1991 <476490767@qq.com>
2016-03-22 12:07:39 +0800

    整理学习目录

--- learning/MDN-canvas/1/js/common.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 100, 100);
-})();
--- learning/css3-learn/animated-hamburger-icons/js/common.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var el = document.querySelectorAll('.hamburger');
-    for (var i = 0; i < el.length; i++) {
-        el[i].addEventListener('click', function() {
-            this.classList.toggle('active');
-        }, false);
-    }
-})();
