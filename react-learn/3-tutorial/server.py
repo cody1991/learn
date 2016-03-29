fc3279b97ae6a3e72efce10e9cd9f4189dc88ad7
cody1991 <476490767@qq.com>
2016-03-29 15:04:35 +0800

    add react learn server.py

--- react-learn/3-tutorial/server.py
+++ react-learn/3-tutorial/server.py
@@ -1,28 +1,54 @@
 #coding=utf-8
 
+# import json
+# import os
+# import time
+# from flask import Flask, Response, request
+
+# app = Flask(__name__, static_url_path='', static_folder='')
+# app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+
+# @app.route('/api/comments', methods=['GET', 'POST'])
+# def comments_handler():
+
+#     with open('comments.json', 'r') as file:
+#         comments = json.loads(file.read())
+
+#     if request.method == 'POST':
+#         newComment = request.form.to_dict()
+#         newComment['id'] = int(time.time() * 1000)
+#         comments.append(newComment)
+
+#         with open('comments.json', 'w') as file:
+#             file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+
+#     return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+
+# if __name__ == '__main__':
+#     app.run(port=int(os.environ.get("PORT",3000)))
+
+
 import json
 import os
 import time
-from flask import Flask, Response, request
+from flask import Flask,Response,request
 
-app = Flask(__name__, static_url_path='', static_folder='')
-app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-@app.route('/api/comments', methods=['GET', 'POST'])
+app = Flask(__name__,static_url_path='',static_folder='')
+app.add_url_rule('/','root',lambda:app.send_static_file('index.html'))
+
+@app.route('/api/comments',methods=['GET','POST'])
 def comments_handler():
-
-    with open('comments.json', 'r') as file:
+    with open('comments.json','r') as file:
         comments = json.loads(file.read())
-
-    if request.method == 'POST':
+    if(request.method == 'POST'):
         newComment = request.form.to_dict()
         newComment['id'] = int(time.time() * 1000)
         comments.append(newComment)
 
-        with open('comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
-
-    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+        with open('comments.json','w') as file:
+            file.write(json.dumps(comments,indent=4,separators=(',',': ')))
+    return Response(json.dumps(comments),mimetype='application/json',headers={'Cache-Control':'no-chche','Access-Control-Allow-Origin':'*'})
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT",3000)))
+    app.run(port=int(os.environ.get('PORT',3000)))
