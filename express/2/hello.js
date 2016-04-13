3db3de080a9c6678de83882212689b50797e1c30
cody1991 <476490767@qq.com>
2016-04-13 12:03:41 +0800

    continue express learn

--- express/2/hello.js
+++ express/2/hello.js
@@ -1,27 +1,45 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+	if (!responseCode) {
+		responseCode = 200;
+	}
+
+	fs.readFile(__dirname + path, function(err, data) {
+		if (err) {
+			res.writeHead(500, {
+				'Content-Type': 'text/plain'
+			});
+			res.end('500 - Internal Error');
+		} else {
+			res.writeHead(responseCode, {
+				'Content-Type': contentType
+			});
+			res.end(data);
+		}
+	});
+}
+
 
 http.createServer(function(req, res) {
+	console.log(1);
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	console.log(path);
 
 	switch (path) {
 		case '':
 		case '/':
-			res.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('Home page');
+			serveStaticFile(res, '/public/home.html', 'text/html');
 			break;
 		case '/about':
-			res.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('About');
+			serveStaticFile(res, '/public/about.html', 'text/html');
+			break;
+		case '/images/btn.png':
+			serveStaticFile(res, '/public/images/btn.png', 'image/png');
 			break;
 		default:
-			res.writeHead(404, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('Not Found');
+			serveStaticFile(res, '/public/notfound.html', 'text/html', 404);
 			break;
 	}
 }).listen(3000);
--- nodejs-in-action/ch2/lib/chat_server.js
+++ nodejs-in-action/ch2/lib/chat_server.js
@@ -17,9 +17,9 @@ exports.listen = function(server) {
 
     // 每个连接的处理逻辑
     io.sockets.on('connection', function(socket) {
-        // guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
 
-        // joinRoom(socket, 'Lobby');
+        joinRoom(socket, 'Lobby');
 
         // // 处理用户消息，更名，以及聊天室的创建和变更
         // handleMessageBroadcasting(socket, nickNames);
@@ -35,4 +35,65 @@ exports.listen = function(server) {
         // handleClientDisconnection(socket, nickNames, namesUsed);
     });
 
+}
+
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    var name = 'Guest ' + guestNumber
+
+    nickNames[socket.id] = name;
+
+    // console.log(nickNames);
+
+    // 让用户知道他们的昵称
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    namesUsed.push(name);
+    return guestNumber + 1;
+}
+
+function joinRoom(socket, room) {
+    socket.join(room);
+
+    currentRoom[socket.id] = room;
+
+    // console.log(currentRoom);
+
+    // 让用户知道他们进入了新房间
+    socket.emit('joinResult', {
+        room: room
+    });
+
+    // 让房间其他用户知道有新用户进入了房间
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + ' has joined ' + room + '.'
+    });
+
+    // console.log(nickNames[socket.id] + ' has joined ' + room + '.');
+
+    // var usersInRoom = io.sockets.adapter.rooms;
+
+    // console.log(usersInRoom);
+
+    // if (usersInRoom.length > 1) {
+    //     var usersInRoomSummary = 'Users currently in ' + room + ': ';
+    //     for (var index in usersInRoom) {
+    //         var userSocketId = usersInRoom[index].id;
+    //         if (userSocketId != socket.id) {
+    //             if (index > 0) {
+    //                 usersInRoomSummary += ', ';
+    //             }
+    //         }
+    //         usersInRoomSummary += nickNames[userSocketId];
+    //     }
+    //     usersInRoomSummary += '.';
+    //     socket.emit('message', {
+    //         text: usersInRoomSummary
+    //     });
+
+    //     console.log(usersInRoomSummary);
+    // }
+
+    // console.log(usersInRoom);
 }
