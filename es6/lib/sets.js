801f420caec6b113bb69c3f9950c653014e5cd10
cody1991 <476490767@qq.com>
2016-04-22 19:09:48 +0800

    learn es6

--- es6/lib/sets.js
+++ es6/lib/sets.js
@@ -8,3 +8,26 @@ console.log(s);
 console.log(s.size);
 
 console.log(s.has('hello'));
+
+var m = new Map();
+
+m.set('hello', 42);
+m.set(s, 34);
+
+console.log(m.get(s));
+
+var wm = new WeakMap();
+
+wm.set(s, {
+    extra: 42
+});
+
+console.log(wm);
+
+var ws = new WeakSet();
+
+ws.add({
+    data: 42
+});
+
+console.log(ws);
