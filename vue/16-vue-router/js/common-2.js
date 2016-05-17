d6735c9d8a7093f03d3e789dc9711470fa73239f
cody1991 <476490767@qq.com>
2016-05-17 20:22:58 +0800

    continue vue router learn

--- vue/16-vue-router/js/common-2.js
+++ vue/16-vue-router/js/common-2.js
@@ -2,6 +2,8 @@ var Foo = Vue.extend({
     template: '<div class="foo">' +
         '<h2>This is Foo!</h2>' +
         '<router-view></router-view>' +
+        "<hr/><p>当前路径：{{$route.path}}" +
+        "<p>当前路由参数：{{$route.params | json}}</p>" +
         '</div>'
 });
 
@@ -25,14 +27,36 @@ router.map({
                 }
             },
             '/bar': {
-                component: Bar
+                component: Bar,
+                auth: true
             },
             '/baz': {
                 component: Baz
             }
         }
+    },
+    '/user/:username/id/:id': {
+        component: {
+            template: '<p>用户名是{{$route.params.username}} id是{{$route.params.id}}'
+        }
     }
-})
+});
+
+// router.beforeEach(function(transition) {
+//     if (transition.to.auth) {
+//         console.log('auth is true');
+//     }
+// });
+
 
 var App = Vue.extend({});
 router.start(App, '#app');
+
+
+// 动态片段只能匹配路径中的一个部分，而全匹配片段则基本类似于它的贪心版。例如 /foo/*bar 会匹配任何以 /foo/ 开头的路径。匹配的部分也会被解析为 $route.params 中的一个键值对。
+
+// 例如:
+
+// 模式  匹配的路径   $route.params
+// /user/*any  /user/a/b/c { any: 'a/b/c' }
+// /foo/*any/bar   /foo/a/b/bar    { any: 'a/b' }
