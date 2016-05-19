c41f11e2997cf771b7835c9a32735786ba9cb29e
cody1991 <476490767@qq.com>
2016-05-19 17:30:31 +0800

    continue vue learn

--- vue/vue-examples/hackernews/src/filters/index.js
+++ vue/vue-examples/hackernews/src/filters/index.js
@@ -9,8 +9,17 @@ export function fromNow(time) {
     const between = Date.now() / 1000 - Number(time);
 
     if (between < 3600) {
-
+        return pluralize(~~(between / 60), ' minute');
+    } else if (between < 86400) {
+        return pluralize(~~(between / 3600), ' hour');
+    } else {
+        return pluralize(~~(between / 86400), 'day');
     }
 }
 
-function plu
+function pluralize(time, label) {
+    if (time === 1) {
+        return time + label;
+    }
+    return time + label + 's';
+}
--- vue/vue-examples/hackernews/src/main.js
+++ vue/vue-examples/hackernews/src/main.js
@@ -1,2 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { domain, fromNow } from './filters'
+import App from './components/App.vue'
+import NewsView from './components/NewsView.vue'
+import ItemView from './components/ItemView.vue'
+import UserView from './components/UserView.vue'
+
+Vue.use(Router);
+
+// 暂时不太懂这两个过滤器
+Vue.filters('fromNow', fromNow);
+Vue.filters('domain', domain);
+
+var router = new Router();
