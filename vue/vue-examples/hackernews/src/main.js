bda77971ee5783ea456130fd9f34261eed9503fa
cody1991 <476490767@qq.com>
2016-05-21 11:11:58 +0800

    continue vue learn

--- vue/vue-examples/hackernews/src/main.js
+++ vue/vue-examples/hackernews/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import { domain, fromNow } from './filters'
+import {
+    domain,
+    fromNow
+}
+from './filters'
 import App from './components/App.vue'
 import NewsView from './components/NewsView.vue'
 import ItemView from './components/ItemView.vue'
@@ -9,7 +13,29 @@ import UserView from './components/UserView.vue'
 Vue.use(Router);
 
 // 暂时不太懂这两个过滤器
-Vue.filters('fromNow', fromNow);
-Vue.filters('domain', domain);
+Vue.filter('fromNow', fromNow);
+Vue.filter('domain', domain);
 
 var router = new Router();
+
+router.map({
+    '/news/:page': {
+        component: NewsView
+    },
+    '/user/:id': {
+        component: UserView
+    },
+    '/item/:id': {
+        component: ItemView
+    }
+});
+
+router.beforeEach(function() {
+    window.scrollTo(0, 0);
+});
+
+router.redirect({
+    '*': '/news/1'
+});
+
+router.start(App, '#app');
