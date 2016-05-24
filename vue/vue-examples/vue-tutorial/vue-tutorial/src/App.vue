16d3d67727b623b3eea3b717726ff78421f6bb78
cody1991 <476490767@qq.com>
2016-05-24 20:56:40 +0800

    continue vue learn

--- vue/vue-examples/vue-tutorial/vue-tutorial/src/App.vue
+++ vue/vue-examples/vue-tutorial/vue-tutorial/src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div id="wrapper">
+      <nav class="navbar navbar-default">
+          <div class="container">
+              <a href="#" class="navbar-brand">
+                  <i class="glyphicon glyphicon-time"></i>
+                  计划表
+              </a>
+              <ul class="nav navbar-nav">
+                  <li>
+                      <a v-link="'/home'">首页</a>
+                  </li>
+                  <li>
+                      <a v-link="'/time-entries'">计划列表</a>
+                  </li>
+              </ul>
+          </div>
+      </nav>
+      <div class="container">
+          <div class="col-sm-3"></div>
+          <div class="col-sm-9">
+              <router-view></router-view>
+          </div>
+      </div>
   </div>
 </template>
-
 <script>
 import Hello from './components/Hello'
 
@@ -33,32 +35,5 @@ export default {
 </script>
 
 <style>
-html {
-  height: 100%;
-}
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
 </style>
--- vue/vue-examples/vue-tutorial/vue-tutorial/src/main.js
+++ vue/vue-examples/vue-tutorial/vue-tutorial/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App'
+import Hello from './components/Hello.vue'
+import Home from './components/Home.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+  /* eslint-disable no-new */
+
+router.map({
+  '/hello': {
+    component: Hello
+  },
+  '/home': {
+    component: Home
+  }
 })
+
+// router.redirect({
+//   '*': '/hello'
+// })
+
+router.start(App, '#app')
