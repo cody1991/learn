02565a8ec0292a385210e7554ce92d49f3af9192
cody1991 <476490767@qq.com>
2016-05-30 17:19:08 +0800

    learn slick.js

---
+++ slick/js/demo-3.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('.demo-3').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
