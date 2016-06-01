10a9a3490b02e34402def3fedef230fa3ac9472f
cody1991 <476490767@qq.com>
2016-06-01 19:41:18 +0800

    这个有点问题，暂时放着

---
+++ vue/vue-examples/cnodejs/src/routers.js
@@ -0,0 +1,68 @@
+'use strict'
+
+export default function(router) {
+    router.map({
+        '/': { //首页
+            name: 'home',
+            component: function(resolve) {
+                require(['./views/index.vue'], resolve);
+            }
+        },
+        // /* 404路由 */
+        // '*': {
+        //     component: function(resolve) {
+        //         require(['./views/index.vue'], resolve);
+        //     }
+        // },
+        // '/cnodevue': { //首页
+        //     name: 'home',
+        //     component: function(resolve) {
+        //         require(['./views/index.vue'], resolve);
+        //     }
+        // },
+        // '/list': { //首页
+        //     name: 'list',
+        //     component: function(resolve) {
+        //         require(['./views/list.vue'], resolve);
+        //     }
+        // },
+        // '/topic/:id': { //专题
+        //     name: 'topic',
+        //     component: function(resolve) {
+        //         require(['./views/topic.vue'], resolve);
+        //     }
+        // },
+        // '/add': { //首页
+        //     name: 'add',
+        //     component: function(resolve) {
+        //         require(['./views/new.vue'], resolve);
+        //     },
+        //     auth: true
+        // },
+        // '/message': { //消息
+        //     name: 'message',
+        //     component: function(resolve) {
+        //         require(['./views/message.vue'], resolve);
+        //     },
+        //     auth: true
+        // },
+        // '/about': { //关于
+        //     name: 'about',
+        //     component: function(resolve) {
+        //         require(['./views/about.vue'], resolve);
+        //     }
+        // },
+        // '/login': { //登录
+        //     name: 'login',
+        //     component: function(resolve) {
+        //         require(['./views/login.vue'], resolve);
+        //     }
+        // },
+        // '/user/:loginname': { //用户信息
+        //     name: 'user',
+        //     component: function(resolve) {
+        //         require(['./views/user.vue'], resolve);
+        //     }
+        // }
+    })
+}
---
+++ vue/vue-examples/cnodejs/src/views/list.vue
@@ -0,0 +1,6 @@
+<template>
+    
+    <section id="page">
+        
+    </section>
+</template>
