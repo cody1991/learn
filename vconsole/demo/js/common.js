c47fdd54538a25beefa8bce695d30bf78584917a
cody1991 <476490767@qq.com>
2016-07-01 17:30:18 +0800

    add new learn

--- vconsole/demo/js/common.js
+++ vconsole/demo/js/common.js
@@ -1,3 +1,22 @@
 (function() {
-    console.log(1);
+    console.log('log');
+    console.info('info');
+    console.debug('debug');
+    console.warn('warn');
+    console.error('bar');
+
+    var obj = {
+        name: 'cody',
+        age: 18
+    }
+
+    console.log(obj);
+
+    var uid = 233;
+    console.log('UserID:', uid);
+
+    // 支持使用 [default|system|...] 的格式将 log 输出到指定 tab 面板：
+
+    console.log('[system]', 'system');
+    console.log('[default]', 'default');
 })();
