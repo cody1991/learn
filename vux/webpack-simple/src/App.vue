e278d86cf6ec44862a2ed159e2e758f0660712dc
cody1991 <476490767@qq.com>
2016-07-04 19:59:28 +0800

    continue vux learn

--- vux/webpack-simple/src/App.vue
+++ vux/webpack-simple/src/App.vue
@@ -1,16 +1,68 @@
 <template>
-  <group>
-    <cell title="vue" value="cool"></cell>
-  </group>
+    <divider>Horizontal flexbox</divider>
+    <flexbox style="height:40px">
+        <flexbox-item>
+            <div class="flex-demo">
+                1
+            </div>
+        </flexbox-item>
+        <flexbox-item>
+            <div class="flex-demo">
+                2
+            </div>
+        </flexbox-item>
+    </flexbox>
+    <divider>Vertical flexbox</divider>
+    <flexbox orient="vertical" :margin-left="0">
+        <flexbox-item>
+            <div class="flex-demo" :margin-left="0">
+                1
+            </div>
+        </flexbox-item>
+        <flexbox-item>
+            <div class="flex-demo" :margin-left="0">
+                2
+            </div>
+        </flexbox-item>
+    </flexbox>
+    <divider>嵌套布局</divider>
+    <flexbox :margin-left="0" style="height:200px;backround-color:#fff" class="vux-1px-tb vux-1px-1 vux-1px-r">
+        <flexbox-item class="vux-1px-r" style="height:200px;">
+        1
+        </flexbox-item>
+        <flexbox-item>
+            <flexbox orient="vertical" :margin-left="0">
+                <flexbox-item class="vux-1px-b">2</flexbox-item>
+                <flexbox-item style="height:100px;">
+                    <flexbox :margin-left="0">
+                        <flexbox-item class="vux-1px-r">3</flexbox-item>
+                        <flexbox-item>4</flexbox-item>
+                    </flexbox>
+                </flexbox-item>
+            </flexbox>
+        </flexbox-item>
+    </flexbox>
+    <divider>Scroller</divider>
+    <scroller height="100px" lock-x style="border:1px solid red" v-ref:scroller>
+        <div><p v-for="i of 10" class="scroller-item">{{i}}</p></div>
+    </scroller>
+    <divider>Scroller</divider>
+    <scroller lock-x scrollbar-y use-pullup use-pulldown height="200px" @pullup:loading="loadMore" @pulldown:loading="refresh" v-if:scroller></scroller>
 </template>
 
 <script>
-import Group from 'vux/dist/components/group';
-import Cell from 'vux/dist/components/cell';
+// import Group from 'vux/dist/components/group';
+// import Cell from 'vux/dist/components/cell';
+// import Divider from 'vux/dist/components/divider';
+// import Flexbox from 'vux/dist/components/flexbox';
+// import FlexboxItem from 'vux/dist/components/flexbox-item';
+import {Divider,Flexbox,FlexboxItem,Scroller} from 'vux'
 export default {
   components:{
-    Group,
-    Cell
+    Divider,
+    Flexbox,
+    FlexboxItem,
+    Scroller
   },
   data () {
     return {
@@ -25,7 +77,15 @@ export default {
 </script>
 
 <style>
-body {
-  font-family: Helvetica, sans-serif;
-}
+@import '~vux/dist/vux.css';
+</style>
+
+<style lang="less">
+  .flex-demo{
+    text-align: center;
+    background-color: #20b907;
+    color:#fff;
+    margin-bottom: 8px;
+    border-radius: 4px;
+  }
 </style>
