87adca49207ec8d169f2a1e31c6816b0edf4695e
cody1991 <476490767@qq.com>
2016-07-12 11:53:59 +0800

    continue es6 learn

--- es6-learn/webpack-es6/es6/main.js
+++ es6-learn/webpack-es6/es6/main.js
@@ -4,9 +4,11 @@ import generators from './generators.js';
 import templateString from './template-string.js';
 import parameters from './parameters.js';
 import destructuring from './destructuring.js';
+import arrowFunctions from './arrowFunctions.js';
 
 iteratorsAndTheForOfLoop();
 generators();
 templateString();
 parameters();
 destructuring();
+arrowFunctions();
