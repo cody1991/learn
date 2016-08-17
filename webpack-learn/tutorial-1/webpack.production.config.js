bb7c213b8f9e434e0444f73aa9c824b63b05fed1
cody1991 <476490767@qq.com>
2016-08-17 18:10:43 +0800

    add webpack learn

---
+++ webpack-learn/tutorial-1/webpack.production.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: __dirname + '/app/main.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.json$/,
+            loader: 'json'
+        }, {
+            test: /\.js$/,
+            excluede: /node_modules/,
+            loader: 'babel',
+            // query: {
+            //     presets: ['es2015', 'react']
+            // }
+        }, {
+            test: /\.css$/,
+            loader: 'style!css?modules!postcss'
+        }]
+    },
+    postcss: [
+        require('autoprefixer')
+    ],
+    plugins: [
+        new webpack.BannerPlugin('Copyright Cody'),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+    ]
+}
