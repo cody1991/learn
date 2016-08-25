89c700a4a22f84ce2f03222be66721f6dead1b6b
cody1991 <476490767@qq.com>
2016-08-25 19:33:50 +0800

    continue canvas game book

--- canvas-game-book/ch-4/demo-1/js/common.js
+++ canvas-game-book/ch-4/demo-1/js/common.js
@@ -10,14 +10,34 @@
 
     function main() {
         // alert('lufylengend start!');
+        // 增加Image的onload事件
         loader = new LLoader();
         loader.addEventListener(LEvent.COMPLETE, loadBitmapdata);
         loader.load('./images/1.jpg', 'bitmapData');
     }
 
     function loadBitmapdata(event) {
+        // loader.content是一个Image
+        // 创建一个LBitmapData对象，并将已读取完的Image作为参数传递
+        // LBitmapData用来保存和读取Image对象，如果要显示图片，需要使用LBitmap
         var bitmapdata = new LBitmapData(loader.content);
+        // 新建LBitmap对象，把上面的LBitmapData对象作为参数传入
+        // LBitmap作用是把Image对象显示在Canvas画板
         var bitmap = new LBitmap(bitmapdata);
-        addChild(bitmap);
+
+        bitmap.x = 50;
+        bitmap.y = 50;
+        bitmap.rotate = 60;
+        bitmap.alpha = 0.4;
+
+        var layer = new LSprite();
+        // 将对象添加到canvas，addChild会按照先后顺序依次显示
+        addChild(layer);
+
+        layer.addChild(bitmap);
     }
 })();
+
+// LBitmapData(image,x,y,width,height);
+
+// LBitmap LBitmap不但可以把图片显示在canvas上，还可以控制图片的各种属性，比如坐标，透明度，旋转，缩放
