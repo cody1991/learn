46905e44aab91cc912780ba484f4f6ad01a55ac8
cody1991 <476490767@qq.com>
2016-09-12 17:41:37 +0800

    continue learnyounode

--- node.js/learnyounode/2.js
+++ node.js/learnyounode/2.js
@@ -1,35 +1,58 @@
-// 你可以通过 process 这个全局对象来获取命令行中的参数。process 对象拥有一个名为 argv的属性，该属性是一个数组，数组中包含了整条命令的所有部分。
-// console.log(process.argv);
+// 要执行一个对文件系统的操作，你将会用到 fs 这个 Node
+//   核心模块。要加载这类核心模块，或者其他的"全局"模块，可以用下面的方式引入：
 
-// 你可以通过 process 这个全局对象来获取命令行中的参数。process 对象
-//  拥有一个名为 argv
-//  的属性，该属性是一个数组，数组中包含了整条命令的所有部分。
+//      var fs = require('fs')
 
-//  首先，请先编写一个包含如下带简易代码的程序来熟悉一下：
+//   现在你可以通过 fs 这个变量来访问整个 fs 模块了。
 
-//     console.log(process.argv)
+//   在 fs 中，所有的同步（或者阻塞）的操作文件系统的方法名都会以 'Sync'
+//   结尾。要读取一个文件，你将需要使用  fs.readFileSync('/path/to/file')
+//   方法。这个方法会返回一个包含文件完整内容的 Buffer 对象。
 
-//  通过执行命令 node program.js
-//  并在后面多加几个参数，来运行我们的程序，比如：
+//   fs 模块的文档，可以使用浏览器打开如下路径来访问：
+//   file://C:\Users\cody\AppData\Roaming\npm\node_modules.learnyounode_npminst
+//   all\learnyounode\3.5.5\learnyounode\node_apidoc\fs.html
 
-//     $ node program.js 1 2 3
+//   Buffer 对象是 Node 用来高效处理数据的方式，无论该数据是 ascii
+//   还是二进制文件，或者其他的格式。Buffer 可以很容易地通过调用 toString()
+//   方法转换为字符串。如：var str = buf.toString()。
 
-//  这样，你就会得到这样一个数组：
+//   Buffer 的文档可以通过浏览器访问如下路径来查看：
+//   file://C:\Users\cody\AppData\Roaming\npm\node_modules.learnyounode_npminst
+//   all\learnyounode\3.5.5\learnyounode\node_apidoc\buffer.html
 
-//     [ 'node', '/path/to/your/program.js', '1', '2', '3' ]
+//   如果你在想如何更简单地去计算行数，请回想一下，一个 JavaScript
+//   字符串，可以使用 .split() 分割成子字符串数组，而且，'\n'
+//   可以作为分隔符。注意，供测试的文件末尾的最后一行并没有进行换行，即没有
+//   '\n' 的存在，因此，使用这个方法的话，所得的数组的长度会比行数多一个。
 
-//  你需要考虑的问题是，如何去循环遍历这些代表数字的参数，从而得到他们的总和。
-//  process.argv 数组的第一个元素永远都会是
-//  node，并且第二个参数总是指向你的程序的路径，所以，你应该从第三个元素（inde
-//  x 是 2）开始，依次累加，直到数组末尾。
 
-//  同时，你需要注意的是，所有 process.argv
-//  中的数组的元素都是字符串类型的，因此，你需要将它们强制转换成数字。你可以通
-//  过加上 + 前缀，或者将其传给 Number() 来解决。例如： +process.argv[2] 或者
-//  Number(process.argv[2])。
+var fs = require('fs');
 
-var sum = 0;
-for (var i = 2; i < process.argv.length; i++) {
-    sum += (+process.argv[i]);
-}
-console.log(sum);
+var fileUrl = process.argv[2];
+
+var buff = fs.readFileSync(fileUrl, 'utf-8');
+
+// console.log(buff);
+
+// buff = buff.toString();
+
+buff = buff.split(/\n/);
+
+// console.log(buff);
+
+// console.log(buff)
+
+console.log(buff.length - 1);
+
+
+// var fs = require('fs')
+
+//     var contents = fs.readFileSync(process.argv[2])
+//     var lines = contents.toString().split('\n').length - 1
+//     console.log(lines)
+
+//     // 只要把 'utf8' 作为 readFileSync 的第二个参数传入
+//     // 就可以不用 .toString() 来得到一个字符串
+//     //
+//     // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
