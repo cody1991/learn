d4cea7f985a08302d2b3d0916c05b1f9b0bc6747
cody1991 <476490767@qq.com>
2016-10-19 16:23:22 +0800

    continue JavaScript ch13

--- JavaScript/Professional-JavaScript/13-事件/EventUtil.js
+++ JavaScript/Professional-JavaScript/13-事件/EventUtil.js
@@ -2,7 +2,7 @@ var EventUtil = {
     addHandler: function(element, type, handler) {
         if (element.addEventListener) {
             element.addEventListener(type, handler, false);
-        } else(element.attachEvent) {
+        } else if (element.attachEvent) {
             element.attachEvent('on' + type, function() {
                 return handler.call(element, window.event);
             });
@@ -19,6 +19,44 @@ var EventUtil = {
             });
         } else {
             element['on' + type] = null;
+        }
+    },
+    getEvent: function(event) {
+        return event ? event : window.event;
+    },
+    getTarget: function(event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    },
+    getRelatedTarget: function(event) {
+        if (event.relatedTarget) {
+            return event.relatedTarget;
+        } else if (event.toElement) {
+            return event.toElement;
+        } else if (event.fromElement) {
+            return event.fromElement;
+        } else {
+            return null;
+        }
+    },
+    getCharCode: function(event) {
+        if (typeof event.charCode == 'number') {
+            return event.charCode;
+        } else {
+            return event.keyCode;
         }
     }
 }
