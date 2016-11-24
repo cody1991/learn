be0dda7e9aaac4f3407694f472d1ba8666c80e2d
cody1991 <476490767@qq.com>
2016-11-24 12:04:25 +0800

    test

--- small-demo/deviceMotionEvent/index.js
+++ small-demo/deviceMotionEvent/index.js
@@ -1,26 +1,29 @@
-if (window.deviceMotionEvent) {
-    window.addEventListener('devicemotion', deviceMotionHandler, false);
-}
+    alert('修改');
 
-var speed = 30;
-var x = 0,
-    y = 0,
-    z = 0,
-    lastX = 0,
-    lastY = 0,
-    lastZ = 0;
-
-function deviceMotionHandler(e) {
-    var acceleration = event.accelerationIncludingGravity;
-    x = acceleration.x;
-    y = acceleration.y;
-    z = acceleration.z;
-    if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed || Math.abs(z - lastZ) > speed) {
-        alert(1);
-        // document.body.appendChild(document.createTextNode('摇一摇'));
+    if (window.deviceMotionEvent) {
+        alert('支持');
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
     }
 
-    lastX = x;
-    lastY = y;
-    lastZ = z;
-}
+    var speed = 30;
+    var x = 0,
+        y = 0,
+        z = 0,
+        lastX = 0,
+        lastY = 0,
+        lastZ = 0;
+
+    function deviceMotionHandler(e) {
+        var acceleration = event.accelerationIncludingGravity;
+        x = acceleration.x;
+        y = acceleration.y;
+        z = acceleration.z;
+        if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed || Math.abs(z - lastZ) > speed) {
+            alert(1);
+            // document.body.appendChild(document.createTextNode('摇一摇'));
+        }
+
+        lastX = x;
+        lastY = y;
+        lastZ = z;
+    }
