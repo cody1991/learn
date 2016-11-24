deb60eba6a7bf690e58716df94bd92be40d8f2ce
cody1991 <476490767@qq.com>
2016-11-24 12:03:23 +0800

    test

--- small-demo/deviceMotionEvent/index.js
+++ small-demo/deviceMotionEvent/index.js
@@ -2,22 +2,6 @@ if (window.deviceMotionEvent) {
     window.addEventListener('devicemotion', deviceMotionHandler, false);
 }
 
-// var speed = 30;//speed
-//         var x = y = z = lastX = lastY = lastZ = 0;
-//         function deviceMotionHandler(eventData) {  
-//           var acceleration =event.accelerationIncludingGravity;
-//                 x = acceleration.x;
-//                 y = acceleration.y;
-//                 z = acceleration.z;
-//                 if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed || Math.abs(z-lastZ) > speed) {
-//                     //简单的摇一摇触发代码
-//                     alert(1);
-//                 }
-//                 lastX = x;
-//                 lastY = y;
-//                 lastZ = z;
-//         }
-
 var speed = 30;
 var x = 0,
     y = 0,
@@ -32,7 +16,8 @@ function deviceMotionHandler(e) {
     y = acceleration.y;
     z = acceleration.z;
     if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed || Math.abs(z - lastZ) > speed) {
-        document.body.appendChild(document.createTextNode('摇一摇'));
+        alert(1);
+        // document.body.appendChild(document.createTextNode('摇一摇'));
     }
 
     lastX = x;
