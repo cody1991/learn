b60ad42c1f2034860ec75631fe88cf720b2a82eb
cody1991 <476490767@qq.com>
2016-12-24 23:34:57 +0800

    continue php learn

--- PHP/README.md
+++ PHP/README.md
@@ -25,3 +25,69 @@
 	if( $express = true ):
 	else:
 	endif;
+
+---
+	
+	(bool) 
+	
+	FALSE
+
+	布尔值 FALSE 本身
+	整型值 0（零）
+	浮点型值 0.0（零）
+	空字符串，以及字符串 "0"
+	不包括任何元素的数组
+	不包括任何成员变量的对象（仅 PHP 4.0 适用）
+	特殊类型 NULL（包括尚未赋值的变量）
+	从空标记生成的 SimpleXML 对象
+
+---
+
+要使用八进制表达，数字前必须加上 0（零）。要使用十六进制表达，数字前必须加上 0x。要使用二进制表达，数字前必须加上 0b。
+
+---
+
+	decimal     : [1-9][0-9]*
+	            | 0
+
+	hexadecimal : 0[xX][0-9a-fA-F]+
+
+	octal       : 0[0-7]+
+
+	binary      : 0b[01]+
+
+	integer     : [+-]?decimal
+	            | [+-]?hexadecimal
+	            | [+-]?octal
+	            | [+-]?binary
+
+---
+
+如果向八进制数传递了一个非法数字（即 8 或 9），则后面其余数字会被忽略。
+
+	<?php
+	var_dump(01090); // 八进制 010 = 十进制 8
+	?>
+
+如果给定的一个数超出了 integer 的范围，将会被解释为 float。同样如果执行的运算结果超出了 integer 范围，也会返回 float
+
+---
+
+一个布尔值 boolean 的 TRUE 被转换成 string 的 "1"。Boolean 的 FALSE 被转换成 ""（空字符串）。这种转换可以在 boolean 和 string 之间相互进行
+
+直接把 array，object 或 resource 转换成 string 不会得到除了其类型之外的任何有用信息。可以使用函数 print_r() 和 var_dump() 列出这些类型的内容。
+
+大部分的 PHP 值可以转变成 string 来永久保存，这被称作串行化，可以用函数 serialize() 来实现
+
+---
+
+	<?php
+	$foo = 1 + "10.5";                // $foo is float (11.5)
+	$foo = 1 + "-1.3e3";              // $foo is float (-1299)
+	$foo = 1 + "bob-1.3e3";           // $foo is integer (1)
+	$foo = 1 + "bob3";                // $foo is integer (1)
+	$foo = 1 + "10 Small Pigs";       // $foo is integer (11)
+	$foo = 4 + "10.2 Little Piggies"; // $foo is float (14.2)
+	$foo = "10.0 pigs " + 1;          // $foo is float (11)
+	$foo = "10.0 pigs " + 1.0;        // $foo is float (11)     
+	?>
--- PHP/index.php
+++ PHP/index.php
@@ -6,6 +6,145 @@
 </head>
 <body>
 	<?php 
+		$str = 'This is a test';
+		$first = $str[0];
+		$third = $str[2];
+		echo $first.'<br/>'.$third.'<br/>';
+
+		$str = 'This is still a test.';
+		$last = $str[strlen($str)-1];
+		echo $last.'<br/>';
+
+		$str = 'Look at the sea';
+		$str[strlen($str)-1] = 'e';
+		echo $str.'<br/>';
+	 ?>
+	<?php  
+		$juices = array('apple','orange','koo'=>'purple');
+
+		error_reporting(E_ALL);
+
+		$great = 'fantastic';
+
+		function greatFun(){
+			return 'great';
+		}
+
+		echo "This is { $great}".'<br/>';
+		echo "This is {$great}".'<br/>';
+		echo "This is ${great}".'<br/>';
+		echo "${greatFun()}".'<br/>';
+
+		class beers{
+			const softdrink = 'rootbeer';
+			public static $ale = 'ipa';
+		}
+
+		$rootbeer = 'A & W';
+		$ipa = 'Ale';
+
+		echo "I'd like an ${beers::softdrink}" . '<br/>';
+		// Fatal error: Access to undeclared static property: beers::$softdrink in /Library/WebServer/Documents/PHP/index.php on line 33
+		// echo "I'd like an {${beers::$softdrink}}" . '<br/>';
+		echo "I'd like an ${beers::$ale}".'<br/>';
+		// Fatal error: Undefined class constant 'ale' in /Library/WebServer/Documents/PHP/index.php on line 36
+		// echo "I'd like an {${beers::ale}}".'<br/>';
+	?>
+	<?php  
+		echo "He drank some $juices[0]".'<br/>';
+		echo "He drank some $juices[koo]".'<br/>';
+		echo "He drank some {$juices['koo']}".'<br/>';
+
+		class people{
+			public $john = 'John';
+			public $jane = 'Jane';
+		}
+
+		$people = new people();
+		echo "$people->john drank some $juices[0]".'<br/>';
+	?>
+	<?php  
+		$str = <<<EOD
+Example of string
+spanning multiple lines
+using heredoc syntax.
+EOD;
+		class foo{
+			var $foo;
+			var $bar;
+			function foo(){
+				$this->foo = 'Foo';
+				$this->bar = array('Bar1','Bar2');
+			}
+		}
+		$foo = new foo();
+		$name = 'MyName';
+
+		echo <<<EOT
+My name is "$name". I am printing some $foo->foo
+Now, I am printing some {$foo->bar[1]}.
+This should print a capital 'A':\x41
+EOT;
+		echo '<br/>';
+		echo <<<'EOT'
+My name is "$name". I am printing some $foo->foo
+Now, I am printing some {$foo->bar[1]}.
+This should print a capital 'A':\x41
+EOT;
+	?>
+	<?php  
+		$a = 1.23456789;
+		$b = 1.23456780;
+		$epsilon = 0.00001;
+		if(abs($a-$b)<$epsilon){
+			echo "true";
+		}
+		echo "<br/>";
+		echo is_nan(NAN);
+		echo "<br/>";
+	?>
+	<?php  
+		$a = 0x1A;
+		echo $a;
+		echo '<br/>';
+		$a = 0123;
+		echo $a;
+		echo '<br/>';
+		echo var_dump(01090);
+		echo '<br/>';
+		echo PHP_INT_MAX;
+		$larger_number = 9223372036854775807;
+		echo '<br/>';
+		var_dump($larger_number);
+
+		$larger_number = $larger_number+1;
+		echo '<br/>';
+		var_dump($larger_number);
+		echo '<br/>';
+		var_dump(25/7);
+		echo '<br/>';
+		var_dump((int)(25/7));
+		echo '<br/>';
+		var_dump(round(25/7));
+		echo '<br/>';
+		echo 0.1+0.7;
+		echo '<br/>';
+
+	?>
+	<hr>
+	<?php  
+		var_dump((bool) "");
+		var_dump((bool) "0");
+		var_dump((bool) -2);
+		var_dump((bool) "foo");
+		var_dump((bool) 2.3e5);
+		var_dump((bool) array(12));
+		var_dump((bool) array());
+		var_dump((bool) "false");
+		var_dump((bool) FALSE);
+	?>
+	<hr>
+	<?php 
 		$a_bool = TRUE;
 		$a_str = "foo";
 		$a_str2 = 'foo';
