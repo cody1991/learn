8145776b90cb552cc700aad50afb0152573c2c45
cody1991 <476490767@qq.com>
2017-01-17 19:10:51 +0800

    完成 javascript ch21

---
+++ JavaScript30/21/README.md
@@ -0,0 +1,1 @@
+[Demo](https://cody1991.github.io/learn/JavaScript30/21/index.html)
---
+++ JavaScript30/21/index.js
@@ -0,0 +1,19 @@
+;
+(function() {
+  'use strict'
+  
+  const arrow = document.querySelector('.arrow')
+  const speed = document.querySelector('.speed-value')
+
+  navigator.geolocation.watchPosition((data) => {
+    console.log(data)
+    
+    speed.textContent = data.coords.speed
+    
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
+  }, (err) => {
+    console.error(err)
+  })
+  
+})();
+
