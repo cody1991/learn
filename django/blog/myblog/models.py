7a0a9ccfdfcb1c890b4846c610fcfc833aaacedb
codytang(唐泽雄) <codytang@tencent.com>
2017-08-04 11:04:49 +0800

    continue django learn

--- django/blog/myblog/models.py
+++ django/blog/myblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 # DateTimeField IntegerField 
@@ -43,6 +44,10 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		# 注意到 URL 配置中的 url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail') ，我们设定的 name='detail' 在这里派上了用场。看到这个 reverse 函数，它的第一个参数的值是 'blog:detail'，意思是 blog 应用下的 name=detail 的函数，由于我们在上面通过 app_name = 'blog' 告诉了 Django 这个 URL 模块是属于 blog 应用的，因此 Django 能够顺利地找到 blog 应用下 name 为 detail 的视图函数，于是 reverse 函数会去解析这个视图函数对应的 URL，我们这里 detail 对应的规则就是 post/(?P<pk>[0-9]+)/ 这个正则表达式，而正则表达式部分会被后面传入的参数 pk 替换，所以，如果 Post 的 id（或者 pk，这里 pk 和 id 是等价的） 是 255 的话，那么 get_absolute_url 函数返回的就是 /post/255/ ，这样 Post 自己就生成了自己的 URL。
+		return reverse('myblog:detail',kwargs={'pk':self.pk})
 
 # python manage.py makemigrations
 # 在 myblog 生成一个0001_initial.py 记录我们队模型做了什么操作
--- django/blog/myblog/views.py
+++ django/blog/myblog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# 注意这里我们用到了从 django.shortcuts 模块导入的 get_object_or_404 方法，其作用就是当传入的 pk 对应的 Post 在数据库存在时，就返回对应的 post，如果不存在，就给用户返回一个 404 错误，表明用户请求的文章不存在。
+from django.shortcuts import render, get_object_or_404
 
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+def index(request):
+	# return render(request,'myblog/index.html',context={
+	# 		'title':'我的博客首页',
+	# 		'welcome':'欢迎访问我的博客首页'
+	# 	})
+	post_list = Post.objects.all().order_by('-created_time')
+	return render(request,'myblog/index.html',context={
+			'post_list':post_list
+		})
+
+def detail(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	return render(request,'myblog/detail.html',context={'post':post})
